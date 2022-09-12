54443f70dc1097ffa34d668f781b5be98bef950a
alexclark <mynameisalexclark@gmail.com>
2022-09-12 08:35:11 -0400

    Moved templates to server.py

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 from flask_app import app
 from flask import render_template, Flask
 app = Flask(__name__)
-@app.route('/main')
+
+
+@app.route('/index')
 def index():
     return render_template('initial_page.html') 
+
+
 if __name__=="__main__":
     app.run(debug=True, port=5001)
